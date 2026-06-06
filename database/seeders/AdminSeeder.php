d0c8fc644c1d281c192a8a1079706eb591806388
Hammam GNF <hammamgonjil@gmail.com>
2026-06-06 09:40:19 +0700

    Add admin user seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            [
+                'email' => 'admin@template.test',
+            ],
+            [
+                'name' => 'Administrator',
+                'password' => bcrypt('123456789'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
