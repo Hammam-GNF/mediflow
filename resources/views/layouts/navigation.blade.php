01591c2e220a05792b28c64a7605f9b0b8c19c2f
Hammam GNF <hammamgonjil@gmail.com>
2026-06-07 10:44:48 +0700

    Fix logo redirect based on user role

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,11 +4,19 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
+                @if(auth()->user()->role === 'admin')
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ route('admin.dashboard') }}">
+                            <x-application-logo class="block h-9 w-auto" />
+                        </a>
+                    </div>
+                @else
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ route('dashboard') }}">
+                            <x-application-logo class="block h-9 w-auto" />
+                        </a>
+                    </div>
+                @endif
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
